65c5fcea6f9c1834e1fcc428214f13e0ab292b67
Keven <keven.santos.sz@gmail.com>
2020-07-11 12:52:03 -0300

    chore: add github package registry

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  'env': {
-    'es6': true,
-    'node': true
+  env: {
+    es6: true,
+    node: true
   },
   plugins: ['@typescript-eslint'],
-  'extends': [
+  extends: [
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
+    'plugin:prettier/recommended'
   ],
-  'globals': {
-    'Atomics': 'readonly',
-    'SharedArrayBuffer': 'readonly'
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
-  'parserOptions': {
-    'ecmaVersion': 2018,
-    'sourceType': 'module'
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
   },
-  'rules': {
+  rules: {
     '@typescript-eslint/no-explicit-any': 0
   }
 }
